16004a4c8590e157fb97d630355422f146b5da8e
yoneya <takashi002013@gmail.com>
2022-05-29 00:44:50 +0000

    feat: ipアドレスを逆引きしてhost名を取得する機能を追加

--- app/api/endpoints/develop.py
+++ app/api/endpoints/develop.py
@@ -3,6 +3,7 @@ from core.logger import get_logger
 from exceptions.core import APIException
 from exceptions.error_messages import ErrorMessage
 from fastapi import APIRouter, Request
+import schemas
 
 logger = get_logger(__name__)
 
@@ -23,6 +24,9 @@ def exec_error2() -> None:
     raise APIException(ErrorMessage.INTERNAL_SERVER_ERROR)
 
 
-@router.get("/request-info")
+@router.get("/request-info", response_model=schemas.RequestInfoResponse)
 def get_request_info(request: Request):
-    return utils.get_request_info(request)
+    ip_address = utils.get_request_info(request)
+    host = utils.get_host_by_ip_address(ip_address)
+
+    return schemas.RequestInfoResponse(ip_address=ip_address, host=host)
