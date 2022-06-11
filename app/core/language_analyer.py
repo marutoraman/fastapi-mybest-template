ad26a702324607a6520fffd642b20e7bd13de92a
yoneya <takashi002013@gmail.com>
2022-06-11 23:29:37 +0000

    feat: sudachi対応のコメント追加

--- app/core/language_analyer.py
+++ app/core/language_analyer.py
@@ -1,15 +1,16 @@
 from typing import Tuple, List
 from enum import Enum
 from sudachipy import tokenizer, dictionary, MorphemeList
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import time
 
 
 class AnalyzedlanguageToken(BaseModel):
-    surface: str
-    dictionaly_form: str
-    reading_form: str
-    part_of_speech: Tuple
+    surface: str = Field(..., description="表層形式(入力文字のまま)")
+    dictionaly_form: str = Field(..., description="辞書形式")
+    reading_form: str = Field(..., description="読みカナ")
+    normalized_form: str = Field(..., description="正規化済の形式")
+    part_of_speech: Tuple = Field(..., description="品詞")
 
 
 class AnalyzedLanguage(BaseModel):
@@ -29,6 +30,14 @@ def tokenize(
     mode: tokenizer.Tokenizer.SplitMode = tokenizer.Tokenizer.SplitMode.C,
     dict_type: SudachiDictType = SudachiDictType.CORE,
 ) -> AnalyzedLanguage:
+    """言語解析
+    Args:
+        - text: 入力テキスト
+        - mode: 分割モード(Cが最もも複合語を考慮します)
+        - dict_type: 辞書の種類(SMALL->CORE->FULLの順で対応する単語が多くなりますが、処理にかかる時間が増加します)
+    Return:
+        解析した言語情報を格納したAnalyzedLanguage形式で返します。
+    """
     start = time.time()
     tokenizer_obj = dictionary.Dictionary(dict_type=dict_type.value).create()
     # mode = tokenizer.Tokenizer.SplitMode.C
@@ -39,6 +48,7 @@ def tokenize(
                 surface=tokenized_obj.surface(),
                 dictionaly_form=tokenized_obj.dictionary_form(),
                 reading_form=tokenized_obj.reading_form(),
+                normalized_form=tokenized_obj.normalized_form(),
                 part_of_speech=tokenized_obj.part_of_speech(),
             )
         )
